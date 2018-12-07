08f52ae9c3dcd03735c8199aad2df4a0fd0c6630
Tassja Blore <njblore@outlook.com>
2018-12-07 19:21:27 +0000

    Updates

---
+++ precache-manifest.d173cbd87096272525230647a40b67a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "85da1e7fcd02c52201f4",
+    "url": "/js-calculator/static/css/main.2b5f9f31.chunk.css"
+  },
+  {
+    "revision": "85da1e7fcd02c52201f4",
+    "url": "/js-calculator/static/js/main.85da1e7f.chunk.js"
+  },
+  {
+    "revision": "89168e69f456fec7e9e1",
+    "url": "/js-calculator/static/js/1.89168e69.chunk.js"
+  },
+  {
+    "revision": "eddb3aa8553140b81695",
+    "url": "/js-calculator/static/js/runtime~main.eddb3aa8.js"
+  },
+  {
+    "revision": "17f76f499355b463609b745bb4c3b8ab",
+    "url": "/js-calculator/static/media/digital-7_mono-webfont.17f76f49.woff2"
+  },
+  {
+    "revision": "1d84ba9124e953f0066daaa6f57251cc",
+    "url": "/js-calculator/static/media/digital-7_mono-webfont.1d84ba91.woff"
+  },
+  {
+    "revision": "dc5b5276549c5a09877117784afc0802",
+    "url": "/js-calculator/index.html"
+  }
+];
